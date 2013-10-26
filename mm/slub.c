6d78b5af29d70e0ea394578ccd03d16e65d3a1db
anarkia1976 <stefano.villa1976@gmail.com>
2013-10-26 15:28:17 +0200

    Revert "mm/slub: set slub_max_order = 0"
    This reverts commit 9b1ff3e5513df82335127101eaf74b93e728c0a3.

--- mm/slub.c
+++ mm/slub.c
@@ -2638,7 +2638,7 @@ EXPORT_SYMBOL(kmem_cache_free);
  * take the list_lock.
  */
 static int slub_min_order;
-static int slub_max_order = 0;
+static int slub_max_order;
 static int slub_min_objects;
 
 /*
