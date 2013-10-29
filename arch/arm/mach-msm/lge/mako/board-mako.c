5846b7e3837a30cceb489d7fb49e670b5b7a39d6
anarkia1976 <stefano.villa1976@gmail.com>
2013-10-29 07:00:08 +0100

    msm: thermal: optimize throttle and polling freqs

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -1333,10 +1333,10 @@ static struct platform_device msm_tsens_device = {
 
 static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 7,
-	.poll_ms = 500,
-	.limit_temp_degC = 80,
-	.temp_hysteresis_degC = 10,
-	.freq_step = 2,
+	.poll_ms = 250,
+	.limit_temp_degC = 85,
+	.temp_hysteresis_degC = 5,
+	.freq_step = 1,
 };
 
 #define MSM_SHARED_RAM_PHYS 0x80000000
